ddc1b3c36627f949d5b39a90f7f6e0c584ce031b
Jasur <spectral.ball@gmail.com>
2021-11-15 01:10:26 +0500

    finished

---
+++ src/components/Column.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col" :style="`flex-basis: ${getFlexBasis}`">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Col',
+    props: {
+      span: Number
+    },
+    computed: {
+      getFlexBasis () {
+        const maxColumnsCount = 24
+        const flexBasisValue = (this.props.span / maxColumnsCount) * 100
+        return this.props.span ? `${flexBasisValue}%` : 'unset'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .col {
+    flex-grow: 1;
+  }
+</style>
